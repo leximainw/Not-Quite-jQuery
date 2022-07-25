c1110e11087d5b8453f95b09beb33b05046f6cde
Alexis Mainwaring <leximainw@gmail.com>
2022-07-25 16:32:05 -0400

    copy filter/map/reduce onto NodeList from Array

--- main.js
+++ main.js
@@ -6,3 +6,6 @@ $.first = function(query) {
     return document.querySelector(query)
 }
 
+NodeList.prototype.filter = Array.prototype.filter
+NodeList.prototype.map = Array.prototype.map
+NodeList.prototype.reduce = Array.prototype.reduce
